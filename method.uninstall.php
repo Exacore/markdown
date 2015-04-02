20bd3b7f6bb6ada56b9433c2a0c81026df7fa172
Exacore <info@exacore.fr>
2015-04-02 15:04:18 +0200

    Update method.uninstall.php

--- method.uninstall.php
+++ method.uninstall.php
@@ -6,7 +6,7 @@ if (!function_exists("cmsms")) exit;
 $this->RemovePreference();
 $this->RemoveSmartyPlugin();
 
-$this->RemoveSmartyPlugi('markdown');
+$this->RemoveSmartyPlugin('markdown');
 
 // put mention into the admin log
 $this->Audit( 0, $this->Lang('friendlyname'), $this->Lang('uninstalled'));
